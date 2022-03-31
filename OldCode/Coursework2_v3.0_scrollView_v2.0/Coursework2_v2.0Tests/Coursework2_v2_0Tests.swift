f627cff78b88011bfba2b8e3b2222a9329fd4e59
O Malley S (FCES) <saidhbh.omalley22@gmail.com>
2022-03-31 19:44:41 +0100

    Update Scroll view

---
+++ OldCode/Coursework2_v3.0_scrollView_v2.0/Coursework2_v2.0Tests/Coursework2_v2_0Tests.swift
@@ -0,0 +1,33 @@
+//
+//  Coursework2_v2_0Tests.swift
+//  Coursework2_v2.0Tests
+//
+//  Created by O Malley S (FCES) on 21/03/2022.
+//
+
+import XCTest
+@testable import Coursework2_v2_0
+
+class Coursework2_v2_0Tests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
